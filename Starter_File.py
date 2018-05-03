15a021a74308876a86a6a69093c34394c17ff326
MattZ45986 <33816924+MattZ45986@users.noreply.github.com>
2018-05-02 21:46:34 -0400

    Update Starter_File.py

--- Starter_File.py
+++ Starter_File.py
@@ -1,3 +1,5 @@
+# Tower Defense
+
 import pygame
 import random
 import math
@@ -18,11 +20,12 @@ class MovingTurret(object):
         pygame.draw.rect(gameDisplay,(255,0,0), (pos[0],pos[1],30,30))
 
 class Turret(object):
-    def __init__(self, pos=(0,0), cost=100, speed=3, damage=8, color=(255,0,0), killCount=0):
+    def __init__(self, game, pos=(0,0), cost=100, speed=3, damage=8, color=(255,0,0), killCount=0):
         self.pos = pos
         self.cost = cost
         self.speed = speed
         self.damage = damage
+        self.game = game
         self.color = color
         self.killCount = killCount
         self.bList = []
@@ -31,8 +34,14 @@ class Turret(object):
         pygame.draw.rect(gameDisplay,(255,0,0), (pos[0],pos[1],30,30))
 
     def fire(self):
-        b = Bullet(self.pos, 0, 3, self)
+        b = Bullet((self.pos[0]+15,self.pos[1]+15), 0, 3, self)
         self.bList.append(b)
+
+    def deleteBullet(self, bullet):
+        try:
+            self.bList.remove(bullet)
+        except ValueError:
+                pass
         
     def getPos(self):
         return self.pos
@@ -63,34 +72,66 @@ class Bullet(object):
         self.turret = turret
         self.direction = direction
 
+    def findTarget(self):
+        target = None
+        dist = 1000
+        for dot in self.turret.game.dList:
+            d = math.sqrt(((dot.pos[0]-self.pos[0]) ** 2) + ((dot.pos[1]-self.pos[1]) ** 2))
+            if  d < dist:
+                target = dot
+                dist = d
+        return target
+
     def updatePos(self):
-        y = self.speed * math.sin(math.radians(self.direction))
-        x = self.speed * math.cos(math.radians(self.direction))
+        dot = self.findTarget()
+        if dot is not None:
+            if (dot.pos[0]-self.pos[0]) != 0: self.direction = math.atan((dot.pos[1]-self.pos[1])/(dot.pos[0]-self.pos[0]))
+            elif (dot.pos[1]-self.pos[1]) > 0: self.direction = math.radians(90)
+            else: self.direction = math.radians(270)
+            if (dot.pos[0]-self.pos[0]) < 0: self.direction += math.pi
+        else: self.turret.deleteBullet(self)
+        y = self.speed * math.sin(self.direction)
+        x = self.speed * math.cos(self.direction)
         self.pos = (self.pos[0] + x, self.pos[1] + y)
+        if dot is not None and ((self.pos[0]-dot.pos[0])**2) + ((self.pos[1]-dot.pos[1])**2) <= dot.radius ** 2:
+            dot.radius -= 3
+            self.turret.deleteBullet(self)
 
     def display(self):
-        if 0 < self.pos[0] < 800:
-            pygame.draw.circle(gameDisplay, (200,200,0), (round(self.pos[0]), round(self.pos[1])), 5)
+        if 0 < self.pos[0] < 800 and 0 < self.pos[1] < 600: pygame.draw.circle(gameDisplay, (200,200,0), (round(self.pos[0]), round(self.pos[1])), 5)
+        else: self.turret.deleteBullet(self)
 
     def getPos(self):
         return self.pos
     def getSpeed(self):
         return self.speed
-    def getTureet(self):
+    def getDirection(self):
+        return self.direction
+    def getTurret(self):
         return self.turret
         
                          
 class Dot(object):
-    def __init__(self,points,speed=1):
+    def __init__(self,game, points,speed=1,radius = 15):
         self.points = points
+        self.game = game
         self.point = 0
+        if radius < 6: radius = 6
+        if radius > 60: radius = 60
+        if radius % 3 != 0: radius -= radius % 3
+        self.radius = radius
         self.pos = list(self.points[self.point])
         self.speed = speed
         
 
     def display(self):
+        if self.radius < 6:
+            self.game.deleteDot(self)
+            return
         if self.point == len(self.points)-1:
-            return True
+            self.game.lives -= 1
+            self.game.deleteDot(self)
+            return
         else:
             yD = self.points[self.point + 1][1] - self.points[self.point][1]
             xD = self.points[self.point + 1][0] - self.points[self.point][0]
@@ -104,7 +145,7 @@ class Dot(object):
                 x = self.speed * xD * (1/math.sqrt(yD ** 2 + xD ** 2))
             self.pos[0] += x
             self.pos[1] += y
-        pygame.draw.circle(gameDisplay, (0,255,0), [int(round(self.pos[0])),int(round(self.pos[1]))],15)
+        pygame.draw.circle(gameDisplay, (0,255,0), [int(round(self.pos[0])),int(round(self.pos[1]))],self.radius)
         xNow = round(self.pos[0])
         xGoal = self.points[self.point + 1][0]
         yNow = round(self.pos[1])
@@ -112,7 +153,11 @@ class Dot(object):
         if (xNow ==  xGoal and  yNow == yGoal) or (self.pos[0] > self.points[self.point +1][0] and x > 0) or (self.pos[0] < self.points[self.point +1][0] and x < 0) or (self.pos[1] < self.points[self.point +1][1] and y < 0) or (self.pos[1] > self.points[self.point +1][1] and y > 0):
             self.point += 1
             self.pos = list(self.points[self.point])
-        return False
+
+    def getSpeed(self):
+        return self.speed
+    def getRadius(self):
+        return self.radius
 
 def intro():
     done = False
@@ -160,72 +205,84 @@ def displayLoss():
         pygame.display.update()
         c += 1
 
-def shoot(tlist):
-    for turret in tlist:
+def shoot(turrets):
+    for turret in turrets:
         turret.fire()
 
-def gameLoop():
-    c = 0
-    lives = 100
-    done = False
-    points =((10,50),(600,50),(600,550),(100,550),(100,150),(400,150),(400,400),(200,400),(200,200),(300,300),(500,300),(300,100),(700,100),(700,570),(50,570),(320,345))
-    parameter = list(points)
-    listy = []
-    tlist = []
-    turret = False
-    pointList = points[:]
-    while done == False:
-        gameDisplay.fill((0,0,0))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            elif event.type == pygame.KEYDOWN:
-                parameter = list(points)
-                if event.key == pygame.K_1: listy.append(Dot(parameter,.1))
-                elif event.key == pygame.K_2: listy.append(Dot(parameter,.2))
-                elif event.key == pygame.K_3: listy.append(Dot(parameter,.3))
-                elif event.key == pygame.K_4: listy.append(Dot(parameter,.4))
-                elif event.key == pygame.K_5: listy.append(Dot(parameter,.5))
-                elif event.key == pygame.K_6: listy.append(Dot(parameter,.6))
-                elif event.key == pygame.K_7: listy.append(Dot(parameter,.7))
-                elif event.key == pygame.K_8: listy.append(Dot(parameter,.8))
-                elif event.key == pygame.K_9: listy.append(Dot(parameter,.9))
-                elif event.key == pygame.K_b: shoot(tlist)
-                else: listy.append(Dot(parameter,3))
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if turret == True:
-                    turret = False
-                    t1 = Turret(pygame.mouse.get_pos())
-                    tlist.append(t1)
-                if 800 > event.pos[0] > 770 and 285 < event.pos[1] < 315:
-                    t1 = MovingTurret()
-                    turret = True
-        for thing in tlist:
-            thing.display(thing.pos)
-            for bullet in thing.bList:
-                bullet.updatePos()
-                bullet.display()
-        if turret == True:
-            t1.display(pygame.mouse.get_pos())
-                
-        pygame.draw.lines(gameDisplay,(255,153,51),False,pointList,10)
-        for dot in listy:
-            if dot is not None:
-                if dot.display():
-                    listy.remove(dot)
-                    lives -= 1
-        displayScore(str(lives) + " lives")
-        pygame.draw.rect(gameDisplay,(255,255,0), (770,285,30,30))
-        pygame.display.update()
-        if lives == 0: return
+class Game(object):
+
+    def __init__(self):
+        self.lives = 100
+        self.points =((10,50),(600,50),(600,550),(100,550),(100,150),(400,150),(400,400),(200,400),(200,200),(300,300),(500,300),(300,100),(700,100),(700,570),(50,570),(320,345))
+        self.dList = []
+        self.tList = []
+
+    def deleteDot(self, dot):
+        self.dList.remove(dot)
+
+    def gameLoop(self):
+        done = False
+        parameter = list(self.points)
+
+        turret = False
+        pointList = self.points[:]
+        c = 0
+        while done == False:
+            gameDisplay.fill((0,0,0))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                elif event.type == pygame.KEYDOWN:
+                    parameter = list(self.points)
+                    if event.key == pygame.K_1: self.dList.append(Dot(self,parameter,.1))
+                    elif event.key == pygame.K_2: self.dList.append(Dot(self,parameter,.2))
+                    elif event.key == pygame.K_3: self.dList.append(Dot(self,parameter,.3))
+                    elif event.key == pygame.K_4: self.dList.append(Dot(self,parameter,.4))
+                    elif event.key == pygame.K_5: self.dList.append(Dot(self,parameter,.5))
+                    elif event.key == pygame.K_6: self.dList.append(Dot(self,parameter,.6))
+                    elif event.key == pygame.K_7: self.dList.append(Dot(self,parameter,.7))
+                    elif event.key == pygame.K_8: self.dList.append(Dot(self,parameter,.8))
+                    elif event.key == pygame.K_9: self.dList.append(Dot(self,parameter,.9))
+                    elif event.key == pygame.K_b: shoot(self.tList)
+                    else: self.dList.append(Dot(self,parameter,3))
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if turret == True:
+                        turret = False
+                        t1 = Turret(self, pygame.mouse.get_pos())
+                        self.tList.append(t1)
+                    if 800 > event.pos[0] > 770 and 285 < event.pos[1] < 315:
+                        t1 = MovingTurret()
+                        turret = True
+            if c % 30 == 0: self.dList.append(Dot(self, parameter, .5,30))
+            if turret == True:
+                t1.display(pygame.mouse.get_pos())
+                    
+            pygame.draw.lines(gameDisplay,(255,153,51),False,pointList,10)
+            for thing in self.tList:
+                thing.display(thing.pos)
+                for bullet in thing.bList:
+                    bullet.updatePos()
+                    bullet.display()
+            for dot in self.dList:
+                if dot is not None:
+                    if dot.display():
+                        self.dList.remove(dot)
+                        self.lives -= 1
+            displayScore(str(self.lives) + " lives")
+            pygame.draw.rect(gameDisplay,(255,255,0), (770,285,30,30))
+            pygame.display.update()
+            c += 1
+            if self.lives == 0: return
+
+    
 ## Main ##
 
 intro()
-gameLoop()
+g = Game()
+g.gameLoop()
 displayLoss()
     
 
 pygame.quit()
 quit()
-
